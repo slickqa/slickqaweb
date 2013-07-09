c2f5290e3c5c349efb28bd1a083ddfadbbd7d763
Jason Corbett <jasoncorbett@gmail.com>
2013-07-08 21:48:17 -0600

    cleanup of some initialization in main.py changing print statements to log statements

--- slickqaweb/main.py
+++ slickqaweb/main.py
@@ -2,30 +2,34 @@ __author__ = 'jcorbett'
 
 from flask import render_template, Response
 from flask_gzip import Gzip
+
 from slickqaweb.app import app
 from slickqaweb.slicklogging import initialize_logging
+from slickqaweb import compiledResources
+
 import os
 import api
-
-import compiledResources
-compiledResources.main_css.seek(0, os.SEEK_END)
-print "main.css is {} bytes in length.".format(compiledResources.main_css.tell())
+import logging
 
 app.config.from_object('slickqaweb.settings')
 if 'SLICK_SETTINGS' in os.environ:
     app.config.from_envvar("SLICK_SETTINGS")
 initialize_logging(app.config)
+
+logger = logging.getLogger("slickqaweb.main")
+logger.info("Slick Web App Server starting...")
 #Gzip(app)
 
+compiledResources.main_css.seek(0, os.SEEK_END)
+logger.debug("main.css is %d bytes in length.", compiledResources.main_css.tell())
 for module in api.modules:
-    print "Found module slickqaweb.api.{}".format(module)
-
+    logger.debug("Found module slickqaweb.api.%s", module)
 
 from mongoengine import connect
-dbname = 'slickij'
-print "Connecting to database: {}".format(dbname)
-connect(dbname)
-
+mongo_hostname = app.config['MONGODB_HOSTNAME']
+mongo_dbname = app.config['MONGODB_DBNAME']
+logger.debug("Connecting to mongo database '%s' on host '%s'.", mongo_dbname, mongo_hostname)
+connect(host=mongo_hostname, db=mongo_dbname)
 
 # initialize with other apis
 @app.route('/', defaults={'path': ''})
