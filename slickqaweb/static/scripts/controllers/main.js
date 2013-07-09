0e7cd7d3ab63335115da4749d01712971502fe31
Jason Corbett <jasoncorbett@gmail.com>
2013-07-08 21:36:37 -0600

    we don't have use for a datamodel in main anymore

--- slickqaweb/static/scripts/controllers/main.js
+++ slickqaweb/static/scripts/controllers/main.js
@@ -9,9 +9,4 @@ angular.module('slickPrototypeApp')
       });
   }])
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   }]);
