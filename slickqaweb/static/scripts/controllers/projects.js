f093be59bfce97fd4c352689c4986c1eee958ba8
Jason Corbett <jasoncorbett@gmail.com>
2013-08-09 17:57:10 -0600

    You can now add a component using a dialog

--- slickqaweb/static/scripts/controllers/projects.js
+++ slickqaweb/static/scripts/controllers/projects.js
@@ -26,14 +26,34 @@ angular.module('slickApp')
         $scope.projectList = {}; // Model for the list header and filter
     }])
     .controller('ViewAndUpdateProjectCtrl', ['$scope', 'NameBasedRestangular', 'NavigationService', '$routeParams', function($scope, rest, nav, $routeParams) {
-        $scope.showDialog = false;
-
-        $scope.toggleShowDialog = function() {
-            $scope.showDialog = !$scope.showDialog;
+        $scope.showAddComponent = false;
+        $scope.component = {
+            name: "",
+            code: ""
         };
 
-        $scope.buttonClicked = function() {
-            $scope.toggleShowDialog();
+        $scope.addComponent = function() {
+            $scope.showAddComponent = !$scope.showAddComponent;
+        };
+
+        $scope.$watch('component.name', function() {
+            $scope.component.code = $scope.component.name.toLowerCase().replace(/ /g, "-");
+        });
+
+        $scope.dialogButtonClicked = function(buttonName) {
+            console.log("Button Name: " + buttonName);
+            if(buttonName == "Add") {
+                if(! $scope.project.components) {
+                    $scope.project.components = [];
+                }
+                $scope.project.components.push({ name: String($scope.component.name), code: String($scope.component.code), id: (new ObjectId()).toString()});
+                $scope.projectForm.$setDirty();
+            } else {
+                //Cancel button was clicked
+            }
+            $scope.component.name = "";
+            $scope.component.code = "";
+            $scope.addComponent();
         };
 
         rest.one('projects', $routeParams.name).get().then(function(project) {
