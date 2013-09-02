54d28cad7c36f4dfdc0f185a31b89a4fe6f3092b
Jason Corbett <jasoncorbett@gmail.com>
2013-09-02 14:56:38 -0600

    Adding more threads to the dev server

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from slickqaweb.main import app
 
 d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
 server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 9000), d)
+server.minthreads = 10
 
 if __name__ == '__main__':
    try:
