8529f0a959356fbda12df864b5cfa6335fb5cec1
Lambson <slambson@SDGL09d5c9fac.local>
2013-09-04 16:39:32 -0600

    Adding the testcase api

---
+++ slickqaweb/api/testcase.py
@@ -0,0 +1,34 @@
+__author__ = 'slambson'
+
+from slickqaweb.app import app
+from slickqaweb.model.testcase import Testcase
+from slickqaweb.model.serialize import deserialize_that
+from flask import Response, request
+from .standardResponses import JsonResponse
+import datetime
+
+# TODO: add error handling. Not sure how to handle that yet.
+@app.route('/api/testcases')
+def get_testcases():
+    return JsonResponse(Testcase.objects)
+
+@app.route('/api/testcases/<testcase_id>')
+def get_project_by_name(testcase_id):
+    return JsonResponse(Testcase.objects(id=testcase_id).first())
+
+
+# @app.route('/api/projects', methods=["POST"])
+# def add_project():
+#     new_project = deserialize_that(request.get_json(), Project())
+#     new_project.lastUpdated = datetime.datetime.utcnow()
+#     new_project.save()
+#     return JsonResponse(new_project)
+#
+#
+# @app.route('/api/projects/<project_name>', methods=["PUT"])
+# def update_project(project_name):
+#     orig = Project.objects(name=project_name).first()
+#     deserialize_that(request.get_json(), orig)
+#     orig.lastUpdated = datetime.datetime.utcnow()
+#     orig.save()
+#     return JsonResponse(orig)
