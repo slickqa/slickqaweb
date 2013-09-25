63f00edc61e2dae187bd97deff4e0b3721bbd6ab
Jason Corbett <jasoncorbett@gmail.com>
2013-09-24 19:40:16 -0600

    making a change in releases so that active vs not active is a property

--- slickqaweb/model/release.py
+++ slickqaweb/model/release.py
@@ -6,5 +6,6 @@ class Release(EmbeddedDocument):
     id = ObjectIdField()
     builds = ListField(EmbeddedDocumentField(Build))
     defaultBuild = StringField()
+    status = StringField(choices=('active', 'inactive'), default='active')
     name = StringField(required=True)
     target = DateTimeField()
