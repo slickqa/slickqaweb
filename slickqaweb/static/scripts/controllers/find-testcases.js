e475c3351e1f03829300e355c167db4e177ca2ed
Jason Corbett <jasoncorbett@gmail.com>
2013-10-21 16:11:35 -0600

    start of find-testcases page

--- slickqaweb/static/scripts/controllers/find-testcases.js
+++ slickqaweb/static/scripts/controllers/find-testcases.js
@@ -16,11 +16,95 @@ angular.module('slickApp')
         nav.addLink('Test Management', 'Find Testcases', 'find-testcases');
     }])
     .controller('FindTestcasesCtrl', ['$scope', 'NameBasedRestangular', 'Restangular', 'NavigationService', '$routeParams', function ($scope, projrest, rest, nav, $routeParams) {
-        $scope.model = {};
-        $scope.model.tags = ["foo", "bar", "hello", "world", "Lee", "Sucks"];
+        // --------------------  required variables initialization ----------------------
+        $scope.testcaseList = {};
 
-        $scope.$watch('model.tags', function() {
-            console.log($scope.model.tags);
+        $scope.queryResult = [];
+
+        $scope.mode = "form";
+
+        $scope.query = "";
+
+        $scope.queryForm = {
+            project: "",
+            name: "",
+            component: "",
+            tags: "",
+            purpose: ""
+        };
+
+        $scope.project = {};
+        $scope.component = {};
+
+        // -------------------- read from query parameters ----------------------
+        if($routeParams.mode == "form") {
+            if($routeParams.project) {
+                $scope.queryForm.project = $routeParams.project;
+            }
+            if($routeParams.name) {
+                $scope.queryForm.name = $routeParams.name;
+            }
+            if($routeParams.component) {
+                $scope.queryForm.component = $routeParams.component;
+            }
+            if($routeParams.tags) {
+                $scope.queryForm.tags = $routeParams.tags;
+            }
+            if($routeParams.purpose) {
+                $scope.queryForm.purpose = $routeParams.purpose;
+            }
+        } else if($routeParams.mode == "query") {
+            $scope.mode = "query";
+            $scope.query = $routeParams.query;
+        }
+
+        window.scope = $scope;
+
+        // -------------------- prepare the form ----------------------
+        $scope.projects = [];
+
+        projrest.all('projects').getList().then(function(projects) {
+            $scope.projects = projects;
+            if($scope.queryForm.project) {
+                _.each($scope.projects, function(project) {
+                    if(project.id == $scope.queryForm.project) {
+                        $scope.project = project;
+                        if ($scope.queryForm.component) {
+                            _.each($scope.project.components, function(component) {
+                                if(component.id == $scope.queryForm.component) {
+                                    $scope.component = component;
+                                }
+                            });
+                        }
+                    }
+                });
+            }
         });
+
+        // -------------------- scope methods ----------------------
+
+        $scope.generateQuery = function() {
+            var parts = [];
+            if ($scope.queryForm.project) {
+                parts.push("eq(project.id,\"" + $scope.queryForm.project + "\")");
+            }
+            if ($scope.queryForm.component) {
+                parts.push("eq(component.id,\"" + $scope.queryForm.component + "\")");
+            }
+            if(parts.length == 1) {
+                $scope.query = parts[0];
+            } else if(parts.length > 1) {
+                $scope.query = "and(" + parts.join(",") + ")"
+            }
+        };
+
+        $scope.generateQuery();
+
+        if($scope.query) {
+            rest.all('testcases').getList({q: $scope.query}).then(function(testcases) {
+                $scope.queryResult = testcases;
+            });
+        }
+
     }]);
 
