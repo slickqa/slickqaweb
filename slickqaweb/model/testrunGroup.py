8fdc9f4acb256ea33b48f5a48a6188a3fd6374c5
Jared Jorgensen <jared2612@gmail.com>
2013-10-22 15:46:33 -0600

    Added testrungroup api and model

---
+++ slickqaweb/model/testrunGroup.py
@@ -0,0 +1,11 @@
+from mongoengine import *
+from projectReference import *
+from .testrun import Testrun
+
+
+class TestrunGroup(Document):
+
+    name = StringField()
+    created = DateTimeField()
+    testruns = ListField(EmbeddedDocumentField(Testrun))
+  
