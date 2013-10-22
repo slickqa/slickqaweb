95638cdd26636befc5e6e1ff8c05fce13d18cba5
Lee Higginson <leeard@gmail.com>
2013-10-22 15:53:33 -0600

    added resultReference

---
+++ slickqaweb/model/resultReference.py
@@ -0,0 +1,9 @@
+from mongoengine import *
+from buildReference import BuildReference
+
+
+class ResultReference(EmbeddedDocument):
+    resultId = ObjectIdField()
+    status = StringField()
+    recorded = DateTimeField()
+    build = EmbeddedDocumentField(BuildReference())
