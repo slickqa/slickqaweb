f6ac1787672ef7149909d0f95eda581f1c0cde5a
Jason Corbett <jasoncorbett@gmail.com>
2013-11-15 11:37:40 -0700

    The testrun summary now complete
    Several computed values were missing from the testrun summary.
    They required a change to the serialization to allow method
    values (computed values) to be serialized.

--- slickqaweb/model/testrunSummary.py
+++ slickqaweb/model/testrunSummary.py
@@ -1,6 +1,7 @@
 __author__ = 'jcorbett'
 
 from mongoengine import *
+from .serialize import serializable
 
 class ResultsByStatus(EmbeddedDocument):
     PASS = IntField(default=0)
@@ -14,3 +15,19 @@ class ResultsByStatus(EmbeddedDocument):
 class TestrunSummary(EmbeddedDocument):
     totalTime = IntField(default=0)
     resultsByStatus = EmbeddedDocumentField(ResultsByStatus, default=ResultsByStatus())
+
+    @serializable
+    def statusListOrdered(self):
+        retval = []
+        for status in ["PASS", "FAIL", "BROKEN_TEST", "NOT_TESTED", "SKIPPED", "NO_RESULT"]:
+            if hasattr(self.resultsByStatus, status) and getattr(self.resultsByStatus, status) > 0:
+                retval.append(status)
+        return retval
+
+    @serializable
+    def Total(self):
+        retval = 0
+        for status in ["PASS", "FAIL", "BROKEN_TEST", "NOT_TESTED", "SKIPPED", "NO_RESULT"]:
+            if hasattr(self.resultsByStatus, status) and getattr(self.resultsByStatus, status) > 0:
+                retval += getattr(self.resultsByStatus, status)
+        return retval
