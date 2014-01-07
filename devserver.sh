62df00a4218ccb0984fadb339cba4a6f006baf15
Jason Corbett <jasoncorbett@gmail.com>
2014-01-06 22:07:49 -0700

    adding update to requirements in devserver startup script

--- devserver.sh
+++ devserver.sh
@@ -7,5 +7,8 @@ SLICK_SETTINGS=`pwd`
 SLICK_SETTINGS="$SLICK_SETTINGS/devserver.cfg"
 
 export SLICK_SETTINGS
+echo "Checking for Updates to requirements..."
+vpy/bin/pip -q install --upgrade -r requirements.txt
 
+echo "Running slick development server (autorestart on file change)..."
 vpy/bin/watchmedo auto-restart -R -d slickqaweb -d slickqawebtest -i '*.swp;*.pyc;catalog' ./server.py
