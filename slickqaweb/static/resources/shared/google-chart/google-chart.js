d78e135bd8fff040eac7872d55044c157939e2b3
Jason Corbett <jasoncorbett@gmail.com>
2014-02-01 22:21:45 -0700

    Adding new chart types (bar, column, line)

--- slickqaweb/static/resources/shared/google-chart/google-chart.js
+++ slickqaweb/static/resources/shared/google-chart/google-chart.js
@@ -28,4 +28,73 @@ angular.module('slickApp')
                 });
             }
         };
+    })
+    .directive('googleBarChart', function() {
+        return {
+            restrict: 'E',
+            transclude: false,
+            template: '<div class="google-bar-chart"></div>',
+            replace: true,
+            scope: {
+                data: "=",
+                options: "="
+            },
+            link: function postLink(scope, element, attrs) {
+                var chart = new google.visualization.BarChart(element[0]);
+                chart.draw(scope.data, scope.options);
+
+                scope.$watch('data', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+                scope.$watch('options', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+            }
+        };
+    })
+    .directive('googleColumnChart', function() {
+        return {
+            restrict: 'E',
+            transclude: false,
+            template: '<div class="google-column-chart"></div>',
+            replace: true,
+            scope: {
+                data: "=",
+                options: "="
+            },
+            link: function postLink(scope, element, attrs) {
+                var chart = new google.visualization.ColumnChart(element[0]);
+                chart.draw(scope.data, scope.options);
+
+                scope.$watch('data', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+                scope.$watch('options', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+            }
+        };
+    })
+    .directive('googleLineChart', function() {
+        return {
+            restrict: 'E',
+            transclude: false,
+            template: '<div class="google-line-chart"></div>',
+            replace: true,
+            scope: {
+                data: "=",
+                options: "="
+            },
+            link: function postLink(scope, element, attrs) {
+                var chart = new google.visualization.LineChart(element[0]);
+                chart.draw(scope.data, scope.options);
+
+                scope.$watch('data', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+                scope.$watch('options', function(newValue, oldValue) {
+                    chart.draw(scope.data, scope.options);
+                });
+            }
+        };
     });
