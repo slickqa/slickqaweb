3a6deddf2b8a678413d614626ae6d36e59fe530b
Jason Corbett <jasoncorbett@gmail.com>
2014-02-02 15:44:52 -0700

    computing testrun info automatically

--- slickqaweb/api/testrun.py
+++ slickqaweb/api/testrun.py
@@ -7,6 +7,7 @@ from slickqaweb.utils import is_provided, is_not_provided
 from slickqaweb.model.testrun import Testrun
 from slickqaweb.model.serialize import deserialize_that
 from slickqaweb.model.query import queryFor
+from .project import get_project, get_release, get_build
 from flask import request, g
 from .standardResponses import JsonResponse, read_request
 from slickqaweb import events
@@ -26,6 +27,22 @@ def add_testrun():
     new_tr = deserialize_that(read_request(), Testrun())
     if is_not_provided(new_tr, 'dateCreated'):
         new_tr.dateCreated = datetime.datetime.utcnow()
+    if is_not_provided(new_tr, 'info') and is_provided(new_tr, 'build') and is_provided(new_tr, 'project') and is_provided(new_tr, 'release'):
+        project = get_project(new_tr.project.name)
+        build = None
+        if project is None:
+            project = get_project(new_tr.project.id)
+        if project is not None:
+            release = get_release(project, new_tr.release.name)
+            if release is None:
+                release = get_release(project, new_tr.release.id)
+            if release is not None:
+                build = get_build(release, new_tr.build.name)
+                if build is None:
+                    build = get_build(release, new_tr.build.id)
+        if build is not None and is_provided(build, 'description'):
+            new_tr.info = build.description
+
     new_tr.save()
     events.CreateEvent(new_tr)
     return JsonResponse(new_tr)
