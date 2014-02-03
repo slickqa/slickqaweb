779334fd8f57494a1c2d62bc4c79e03cd80c3f28
Jason Corbett <jasoncorbett@gmail.com>
2014-02-03 11:54:30 -0700

    forgot orderby when I limited

--- slickqaweb/static/resources/pages/testruns/testruns.js
+++ slickqaweb/static/resources/pages/testruns/testruns.js
@@ -96,12 +96,12 @@ angular.module('slickApp')
             q = q + ")";
         }
         if (!q) {
-            rest.all('testruns').getList({limit: 500}).then(function(testruns) {
+            rest.all('testruns').getList({orderby: '-recorded', limit: 500}).then(function(testruns) {
                 $scope.testruns = testruns;
                 window.testruns = testruns;
             });
         } else {
-            rest.all('testruns').getList({q: q, limit: 500}).then(function(testruns) {
+            rest.all('testruns').getList({q: q, limit: 500, orderby: '-recorded'}).then(function(testruns) {
                 $scope.testruns = testruns;
                 window.testruns = testruns;
             });
