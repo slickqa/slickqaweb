ecd67951aa18c9967b6a2eab0ea75a9ec375880d
Jason Corbett <jasoncorbett@gmail.com>
2014-02-09 20:23:01 -0700

    Adding automatic testrun group system configuration

---
+++ slickqaweb/model/systemConfiguration/automaticTestrunGroup.py
@@ -0,0 +1,29 @@
+__author__ = 'jcorbett'
+
+
+from mongoengine import *
+
+from .types import AllTypes
+
+AutomaticTestrunGroupSystemConfigurationType = "auto-add-to-testrungroup"
+
+
+class AutomaticTestrunGroupSystemConfiguration(Document):
+    meta = {'collection': 'system-configurations'}
+    name = StringField()
+    enabled = BooleanField()
+    configurationType = StringField(required=True, default=AutomaticTestrunGroupSystemConfigurationType, choices=[AutomaticTestrunGroupSystemConfigurationType,])
+    template = StringField()
+    matchers = ListField(EmbeddedDocumentField(TestrunMatch))
+
+    @queryset_manager
+    def objects(doc_cls, queryset):
+        """Custom QuerySet Manager that filters based on the configurationType"""
+        return queryset.filter(configurationType=AutomaticTestrunGroupSystemConfigurationType)
+
+
+class TestrunMatch(EmbeddedDocument):
+    propertyName = StringField()
+    propertyValue = StringField()
+
+AllTypes[AutomaticTestrunGroupSystemConfigurationType] = AutomaticTestrunGroupSystemConfiguration
