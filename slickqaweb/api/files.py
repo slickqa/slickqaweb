c904d50537df085829f30314b4246977b400067b
Jason Corbett <jasoncorbett@gmail.com>
2014-03-03 14:56:01 -0700

    the file api now supports ranged requests

--- slickqaweb/api/files.py
+++ slickqaweb/api/files.py
@@ -8,6 +8,8 @@ from slickqaweb.model.fileChunk import FileChunk
 from slickqaweb.model.serialize import deserialize_that
 from .standardResponses import JsonResponse, read_request
 from hashlib import md5
+import re
+import logging
 
 @app.route("/api/files/<file_id>")
 def get_stored_file(file_id):
@@ -58,15 +60,72 @@ def add_chunk_to_file(file_id):
     stored_file.save()
     return JsonResponse(stored_file)
 
+#@app.route("/api/files/<file_id>/content/<filename>", methods=["HEAD"])
+#def get_header_for_file(file_id, filename):
+#    logger = logging.getLogger('slickqaweb.api.files.get_header_for_file')
+#    stored_file = StoredFile.objects(id=ObjectId(file_id)).first()
+#    if stored_file is None:
+#        return Response("File with id '{}' and name '{}' not found.".format(file_id, filename), mimetype="text/plain", status=404)
+#    logger.debug("Returning header information for file with id {} and name {}".format(file_id, filename))
+
+
+
 @app.route("/api/files/<file_id>/content/<filename>")
 def get_file_content(file_id, filename):
+    logger = logging.getLogger('slickqaweb.api.files.get_file_content')
     stored_file = StoredFile.objects(id=ObjectId(file_id)).first()
     if stored_file is None:
-        return Response("File with id '{}' not found.", mimetype="text/plain", status=404)
-    def write_chunks():
+        return Response("File with id '{}' and name '{}' not found.".format(file_id, filename), mimetype="text/plain", status=404)
+
+    range_header = request.headers.get('Range', None)
+    response = None
+    if not range_header:
+        logger.info("Returning file in classic mode")
+        def write_chunks():
+            for chunk in FileChunk.objects(files_id=stored_file.id).order_by('+n'):
+                yield chunk.data
+        response = Response(write_chunks(), mimetype=stored_file.mimetype, direct_passthrough=True)
+    else:
+        logger.debug("Returning file with id {} and filename {} and md5sum {} in ranged mode.".format(file_id, filename, stored_file.md5))
+        byte1, byte2 = 0, (stored_file.length - 1)
+
+        m = re.search('(\d+)-(\d*)', range_header)
+        g = m.groups()
+
+        if g[0]:
+            byte1 = int(g[0])
+        if g[1]:
+            possible_byte2 = int(g[1])
+            if possible_byte2 < byte2:
+                byte2 = possible_byte2
+
+        data = []
+        start_chunk_number = byte1 / stored_file.chunkSize
+        end_chunk_number = byte2 / stored_file.chunkSize
+        if byte2 % stored_file.chunkSize > 0:
+            end_chunk_number += 1
+        start_index = byte1 % stored_file.chunkSize
+        end_index = byte2 % stored_file.chunkSize
+
+        logger.debug("Using range information {}-{}/{}, chunks {}:{}-{}:{}".format(byte1, byte2, stored_file.length - 1, start_chunk_number, start_index, end_chunk_number, end_index))
+
         for chunk in FileChunk.objects(files_id=stored_file.id).order_by('+n'):
-            yield chunk.data
-    return Response(write_chunks(), mimetype=stored_file.mimetype)
+            if chunk.n >= start_chunk_number and chunk.n <= end_chunk_number:
+                start = 0
+                end = stored_file.chunkSize
+                if chunk.n == start_chunk_number:
+                    start = start_index
+                if chunk.n == end_chunk_number:
+                    end = end_index
+                data.extend(chunk.data[start:end])
+
+        logger.debug("Ranged request length is {} bytes.".format(len(data)))
+
+        response = Response(data, 206, mimetype=stored_file.mimetype, direct_passthrough=True)
+        response.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte2, stored_file.length))
+
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
 
 
 
