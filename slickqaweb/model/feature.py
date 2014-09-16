4f964c004201692dbafac6e59144a20b5381e7dc
Jason Corbett <jasoncorbett@gmail.com>
2014-09-15 19:59:10 -0600

    adding images to features

--- slickqaweb/model/feature.py
+++ slickqaweb/model/feature.py
@@ -1,7 +1,9 @@
 from mongoengine import *
-
+from storedFile import StoredFile
 
 class Feature(EmbeddedDocument):
     id = ObjectIdField()
     name = StringField()
     description = StringField()
+    img = ReferenceField(StoredFile, dbref=True)
+    imgUrl = StringField()
