ba8437cc3cd00d9f8919e63f761588c066dbc9ec
Jason Corbett <jasoncorbett@gmail.com>
2018-07-17 17:29:53 -0600

    fixing add-indexes to work with new docker container

--- add-indexes.py
+++ add-indexes.py
@@ -5,74 +5,85 @@ import sys
 
 prod_config='/opt/slick/prodserver.cfg'
 
-hostName=""
-dbName=""
-
+config = {}
 with open(prod_config) as f:
     for line in f:
         if line.startswith("MONGODB_HOSTNAME"):
-            hostName = line.split('=')[1].strip()
+            config['MONGODB_HOSTNAME'] = line.split('=')[1].strip().strip('"')
         if line.startswith("MONGODB_DBNAME"):
-            dbName = line.split('=')[1].strip()
+            config['MONGODB_DBNAME'] = line.split('=')[1].strip().strip('"')
+        if line.startswith("MONGODB_USERNAME"):
+            config['MONGODB_USERNAME'] = line.split('=')[1].strip().strip('"')
+        if line.startswith("MONGODB_PASSWORD"):
+            config['MONGODB_PASSWORD'] = line.split('=')[1].strip().strip('"')
+        if line.startswith("MONGODB_AUTHDB"):
+            config['MONGODB_AUTHDB'] = line.split('=')[1].strip().strip('"')
 
-hostName = hostName.replace("\"", "")
-dbName = dbName.replace("\"", "")
+connect_options = {
+        "host": config['MONGODB_HOSTNAME'],
+}
+if 'MONGODB_USERNAME' in config:
+    connect_options['username'] = config['MONGODB_USERNAME']
+if 'MONGODB_PASSWORD' in config:
+    connect_options['password'] = config['MONGODB_PASSWORD']
+if 'MONGODB_AUTHDB' in config:
+    connect_options['authSource'] = config['MONGODB_AUTHDB']
 
-client = pymongo.MongoClient(host=hostName, port=27017)
-db = client[dbName]
+client = pymongo.MongoClient(**connect_options)
+db = client[config['MONGODB_DBNAME']]
 
 sys.stdout.write("Creating index on projects...")
 sys.stdout.flush()
-db.projects.ensure_index([("name", pymongo.ASCENDING)])
+db.projects.create_index([("name", pymongo.ASCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating indexes on configurations...")
 sys.stdout.flush()
-db.configurations.ensure_index([("name", pymongo.ASCENDING)])
-db.configurations.ensure_index([("configurationType", pymongo.ASCENDING)])
-db.configurations.ensure_index([("filename", pymongo.ASCENDING)])
+db.configurations.create_index([("name", pymongo.ASCENDING)])
+db.configurations.create_index([("configurationType", pymongo.ASCENDING)])
+db.configurations.create_index([("filename", pymongo.ASCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating indexes on testcases...")
 sys.stdout.flush()
-db.testcases.ensure_index([("automationId", pymongo.ASCENDING)])
-db.testcases.ensure_index([("automationKey", pymongo.ASCENDING)])
-db.testcases.ensure_index([("project.id", pymongo.ASCENDING), ("component.id", pymongo.ASCENDING)])
-db.testcases.ensure_index([("tags", pymongo.ASCENDING)])
+db.testcases.create_index([("automationId", pymongo.ASCENDING)])
+db.testcases.create_index([("automationKey", pymongo.ASCENDING)])
+db.testcases.create_index([("project.id", pymongo.ASCENDING), ("component.id", pymongo.ASCENDING)])
+db.testcases.create_index([("tags", pymongo.ASCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating indexes on results (could take a while)...")
 sys.stdout.flush()
-db.results.ensure_index([("runstatus", pymongo.ASCENDING), ("hostname", pymongo.ASCENDING)])
-db.results.ensure_index([("runstatus", pymongo.ASCENDING), ("testcase.automationTool", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
-db.results.ensure_index([("testrun.testrunId", pymongo.ASCENDING), ("status", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
-db.results.ensure_index([("build.buildId", pymongo.ASCENDING), ("status", pymongo.ASCENDING)])
-db.results.ensure_index([("testcase.testcaseId", pymongo.ASCENDING), ("config.configId", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
-db.results.ensure_index([("testcase.testcaseId", pymongo.ASCENDING), ("config.configId", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING), ("testrun.testplanId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
-db.results.ensure_index([("testcase.testcaseId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
+db.results.create_index([("runstatus", pymongo.ASCENDING), ("hostname", pymongo.ASCENDING)])
+db.results.create_index([("runstatus", pymongo.ASCENDING), ("testcase.automationTool", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
+db.results.create_index([("testrun.testrunId", pymongo.ASCENDING), ("status", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
+db.results.create_index([("build.buildId", pymongo.ASCENDING), ("status", pymongo.ASCENDING)])
+db.results.create_index([("testcase.testcaseId", pymongo.ASCENDING), ("config.configId", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
+db.results.create_index([("testcase.testcaseId", pymongo.ASCENDING), ("config.configId", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING), ("testrun.testplanId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
+db.results.create_index([("testcase.testcaseId", pymongo.ASCENDING), ("recorded", pymongo.DESCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating indexes on testruns...")
 sys.stdout.flush()
-db.testruns.ensure_index([("project.id", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING)])
-db.testruns.ensure_index([("project.id", pymongo.ASCENDING), ("dateCreated", pymongo.DESCENDING)])
-db.testruns.ensure_index([("build.buildId", pymongo.ASCENDING), ("dateCreated", pymongo.DESCENDING)])
+db.testruns.create_index([("project.id", pymongo.ASCENDING), ("release.releaseId", pymongo.ASCENDING)])
+db.testruns.create_index([("project.id", pymongo.ASCENDING), ("dateCreated", pymongo.DESCENDING)])
+db.testruns.create_index([("build.buildId", pymongo.ASCENDING), ("dateCreated", pymongo.DESCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating index on system-configurations...")
 sys.stdout.flush()
-db['system-configurations'].ensure_index([("configurationType", pymongo.ASCENDING), ("name", pymongo.ASCENDING)])
+db['system-configurations'].create_index([("configurationType", pymongo.ASCENDING), ("name", pymongo.ASCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
 sys.stdout.write("Creating indexes on fs.chunks...")
 sys.stdout.flush()
-db.projects.ensure_index([("files_id", pymongo.ASCENDING), ("n", pymongo.ASCENDING)])
+db['fs.chunks'].create_index([("files_id", pymongo.ASCENDING), ("n", pymongo.ASCENDING)])
 sys.stdout.write("Done.\n")
 sys.stdout.flush()
 
