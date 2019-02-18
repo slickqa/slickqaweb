943bd89614bbdba624bef21dca7660d52c3d167b
Steve Jensen <steve.jensen@vivint.com>
2019-02-17 23:02:18 -0700

    pretty confident haha

--- slickqaweb/api/result.py
+++ slickqaweb/api/result.py
@@ -644,7 +644,11 @@ def get_single_scheduled_result(hostname):
 @app.route('/api/results/queue/statistics')
 def get_queue_statistics():
     conn = connection.get_connection()
-    result = conn[app.config['MONGODB_DBNAME']]['results'].aggregate([{'$match': {'status': 'NO_RESULT', 'runstatus': 'SCHEDULED'}}, {'$group': {'_id': {'requirements': '$requirements', 'project': '$project.name', 'release': '$release.name', 'build': '$build.name'}, 'date': {'$last': '$recorded'}, 'oldest_result': {'$first': '$recorded'}, 'count': {'$sum': 1}}}])
+    match = {'status': 'NO_RESULT', 'runstatus': 'SCHEDULED'}
+    if 'minutes-scheduled' in request.args:
+        minutes_scheduled = int(request.args['minutes-scheduled'])
+        match['recorded'] = {'$lt': datetime.datetime.utcnow() + datetime.timedelta(minutes=-minutes_scheduled)}
+    result = conn[app.config['MONGODB_DBNAME']]['results'].aggregate([{'$match': match}, {'$group': {'_id': {'requirements': '$requirements', 'project': '$project.name', 'release': '$release.name', 'build': '$build.name'}, 'date': {'$last': '$recorded'}, 'oldest_result': {'$first': '$recorded'}, 'count': {'$sum': 1}}}])
     return JsonResponse(list(result))
 
 @app.route('/api/results/queue/running')
